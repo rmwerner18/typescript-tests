c13fc7227bafd07b2afd246d8e491af95ecdd681
humorous-storage-7521 <rmwerner@umich.edu>
2021-03-03 16:55:41 -0600

    try generics and unions

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,32 @@ const App = () => {
     id: number;
   }
 
+  class UserClass {
+    username: string 
+    id: number 
+    size: number 
+
+
+    constructor(username: string, id: number, size: number) {
+      this.username = username;
+      this.id = id;
+      this.size = size
+    }
+  }
+
+  type num = 1 | 2 | 4 | 7
+  type stringArray = Array<string>
+  type ObjectWithNameArray = Array<{ name: string }>
+
+  let arr: Array<string> = ['a', 'c']
+  let arr2: ObjectWithNameArray = [{name: "josh"}, {name: 'stu'}, {name: 'greg'}]
+
+  function number(): num  {
+    return 4
+  }
+
+  const todd = new UserClass('todd', 2, 3)
+
   const mike: User = {
     username: 'mike',
     id: 7
@@ -21,6 +47,7 @@ const App = () => {
     <h1>
       {mike.username}<br/>
       {jay.username}
+      {todd.size}
     </h1>
   )
 
