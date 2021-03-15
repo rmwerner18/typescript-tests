91d8d4a555851cadfa7a597dbb9bf03828d96d95
humorous-storage-7521 <rmwerner@umich.edu>
2021-03-15 16:01:15 -0500

    utility types

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ const App = () => {
   interface User {
     username: string;
     id: number;
+    size: number
   }
+
+  type UserNameAndSize = Omit<User, 'id'>
 
   class UserClass {
     username: string 
@@ -20,20 +23,32 @@ const App = () => {
     }
   }
 
+  function wrapInArray(obj: string | string[]) {
+    if (typeof obj === "string") {
+      return [obj];
+  //          ^ = (parameter) obj: string
+    } else {
+      return obj;
+    }
+  }
+
+  let abcd = ['sdfgsdfg', 'sdfbvdsfb', 'sdfgdsf']
+
+
   type num = 1 | 2 | 4 | 7
-  type stringArray = Array<string>
-  type ObjectWithNameArray = Array<{ name: string }>
+  type abc = Array<string>
+  type ObjectWithNameArray = Array<{ name: string, age: number }>
 
-  let arr: Array<string> = ['a', 'c']
-  let arr2: ObjectWithNameArray = [{name: "josh"}, {name: 'stu'}, {name: 'greg'}]
+  let arr: abc = ['a', 'c']
+  let arr2: ObjectWithNameArray = [{name: "josh", age: 8}, {name: 'stu', age: 12}, {name: 'greg', age: 32}]
 
-  function number(): num  {
-    return 4
+  function number(): string  {
+    return "hello world"
   }
 
   const todd = new UserClass('todd', 2, 3)
 
-  const mike: User = {
+  const mike: Partial<User> = {
     username: 'mike',
     id: 7
   }
@@ -43,11 +58,22 @@ const App = () => {
     id: 8
   }
 
+  let martin: UserNameAndSize = {
+    username: "martin",
+    size: 4
+  }
+
+  let george: Omit<User, 'id'> = {
+    username: 'george',
+    size: 3
+  }
+
   return (
     <h1>
       {mike.username}<br/>
       {jay.username}
-      {todd.size}
+      {todd.size}<br/>
+      {wrapInArray(abcd)}
     </h1>
   )
 
